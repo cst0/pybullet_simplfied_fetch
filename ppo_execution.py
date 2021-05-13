f6e6a51423a1c355fa6e4ccbdd5ee646aba88fec
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-05-13 18:43:53 -0400

    ppo agent

---
+++ ppo_execution.py
@@ -0,0 +1,23 @@
+import gym
+from randomagent import RandomAgent
+from simple_grasping.standard_interfaces import *
+
+def main():
+    agent = RandomAgent()
+    env = gym.make('SimpleFetch-v0')
+    env.place_objects([
+            Block.SMALL,
+            Block.MEDIUM,
+            Block.LARGE
+        ])
+    #env.place_objects([Block.SMALL], [Pose(1, 1, 1)])
+    observation = env.reset()
+    finish = False
+    while not finish:
+        action = agent.choose_action(observation)
+        observation, _, finish, _ = env.step(action)
+        if finish:
+            print("Told to finish-- loop will now terminate")
+
+if __name__ == '__main__':
+    main()
---
+++ ppoagent.py
@@ -0,0 +1,15 @@
+from simple_grasping import standard_interfaces
+import numpy as np
+from baselines.baselines import ppo2
+
+class RandomAgent:
+    def __init__(self):
+        pass
+
+    def choose_action(self, observation:standard_interfaces.Observation) -> standard_interfaces.Action:
+        a = standard_interfaces.Action(
+                _x_dist=np.random.uniform(-0.4, 0.4),
+                _y_dist=np.random.uniform(-0.4, 0.4),
+                _z_interact=(np.random.random() < 0.05) # x in 100 chance of interacting
+            )
+        return a
